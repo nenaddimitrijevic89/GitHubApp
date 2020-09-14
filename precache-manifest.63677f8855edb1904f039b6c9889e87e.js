a543749e6d4e063b92b20e6f466f434dbfaf3bbe
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2020-09-14 12:47:20 +0200

    Updates

---
+++ precache-manifest.63677f8855edb1904f039b6c9889e87e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42da42ede3c96b756836afbfb261e230",
+    "url": "/GitHubApp/index.html"
+  },
+  {
+    "revision": "b7be64571671622ad420",
+    "url": "/GitHubApp/static/css/main.36940d44.chunk.css"
+  },
+  {
+    "revision": "9eb1babf2f5ab61cb4b6",
+    "url": "/GitHubApp/static/js/2.5f06a5fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GitHubApp/static/js/2.5f06a5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7be64571671622ad420",
+    "url": "/GitHubApp/static/js/main.2e708d3d.chunk.js"
+  },
+  {
+    "revision": "81fbccef5bef8bf93103",
+    "url": "/GitHubApp/static/js/runtime-main.3d2b596b.js"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/GitHubApp/static/media/logo.3e54ed15.png"
+  },
+  {
+    "revision": "90f7e49c4a86b04c2b2c5389d49f91bc",
+    "url": "/GitHubApp/static/media/user.90f7e49c.png"
+  }
+]);
