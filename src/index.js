d1678d30e5857efd1eea3a6239c4468231792fb6
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-01-13 15:00:32 +0100

    fetching users data async

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './view/App';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import './index.css';
+import App from './view/App';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/view/App.js
+++ src/view/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Users } from './components/Users/Users';
+import Users from './components/Users/Users';
 import { Repositories } from './components/Repositories/Repositories';
 import { SingleRepo } from './components/Repositories/SingleRepo/SingleRepo';
 
@@ -13,6 +13,6 @@ function App() {
         <Route exact path='/repositories/:id/:id' component={SingleRepo} />
     </Switch>
   );
-}
+};
 
 export default App;
